275e162a0ec0d6190fc65f7191c70afe55ba58c0
benji3106 <benjamin3106@live.fr>
2025-10-21 15:49:57 +0200

    fix: modify C program that prints the last digit of a random number

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 
 	if (lastDigit > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5\n", n, lastDigit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
 	}
 	else if (lastDigit == 0)
 	{
-		printf("Lasst digit of %d is %d and is 0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is 0\n", n, lastDigit);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not0\n", n, lastDigit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 	}
 
 	return (0);
