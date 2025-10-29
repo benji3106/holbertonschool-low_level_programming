a9b86b3c537c833ae38b3e3bb0fc60a02a59fc35
benji3106 <benjamin3106@live.fr>
2025-10-29 09:39:49 +0100

    feat: create function that prints half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * puts_half -  prints half of a string.
+ * @str: the character to print
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int length = 0;
+	int start;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (length % 2 == 0)
+	{
+		start = length / 2;
+	}
+	else
+	{
+		start = (length + 1) / 2;
+	}
+
+	for (i = start; i < length; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
