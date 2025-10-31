a415bd7f39fda788ba18be7c56fe1115e8579feb
benji3106 <benjamin3106@live.fr>
2025-10-31 14:48:04 +0100

    feat: create function that changes all lowercase letters of a string to uppercase

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void reverse_array(int *a, int n);
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -17,6 +16,8 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *s);
 
 
 #endif
